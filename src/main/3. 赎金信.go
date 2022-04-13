691cf7bc0d01c3073137ae84223a6ae29c477756
hn hn <2125102013@stu.hqu.edu.cn>
2022-04-13 23:39:31 +0800

    4.13

---
+++ src/main/3. 赎金信.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//字符统计
+func canConstruct(ransomNote, magazine string) bool {
+	if len(ransomNote) != len(magazine) {
+		return false
+	}
+	//ascii码
+	cnt := [26]int{}
+	for _, ch := range magazine {
+		cnt[ch-'a']++
+	}
+	for _, ch := range ransomNote {
+		cnt[ch-'a']--
+		if cnt[ch-'a'] < 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Print(canConstruct("aaa", "aab"))
+}
