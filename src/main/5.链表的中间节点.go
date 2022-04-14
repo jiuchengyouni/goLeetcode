408136be9336bad24d8cdfbe83e1fce9b1dc4fcc
hn hn <2125102013@stu.hqu.edu.cn>
2022-04-15 01:00:26 +0800

    4.15

---
+++ src/main/5.链表的中间节点.go
@@ -0,0 +1,23 @@
+package main
+
+//快慢指针
+
+type ListNode2 struct {
+	Val  int
+	Next *ListNode2
+}
+
+func middleNode(head *ListNode2) *ListNode2 {
+	if head.Next == nil {
+		return head
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}
+func main() {
+
+}
