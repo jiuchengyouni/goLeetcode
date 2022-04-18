bd043670ab2635c95c77069868343bc085fa7ce0
hn hn <2125102013@stu.hqu.edu.cn>
2022-04-18 19:22:02 +0800

    4.18

---
+++ src/main/9.二分查找.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	first := 0
+	last := len(nums) - 1
+	for first <= last {
+		mid := (last-first)/2 + first
+		if nums[mid] == target {
+			return mid + 1
+		} else if nums[mid] > target {
+			last = mid - 1
+		} else {
+			first = mid + 1
+		}
+
+	}
+	return -1
+}
+
+func main() {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	fmt.Println(search(nums, 9))
+}
