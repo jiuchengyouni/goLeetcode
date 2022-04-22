2ff50158900b57f393eafd9b559cdfd211d487c7
hn hn <2125102013@stu.hqu.edu.cn>
2022-04-22 10:38:33 +0800

    4.22

---
+++ src/main/18.删除链表的倒数第N个节点.go
@@ -0,0 +1,23 @@
+package main
+
+//19
+type ListNode18 struct {
+	Val  int
+	Next *ListNode18
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	first, second := head, dummy
+	for i := 0; i < n; i++ {
+		first = first.Next
+	}
+	for ; first != nil; first = first.Next {
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
+}
+func main() {
+
+}
