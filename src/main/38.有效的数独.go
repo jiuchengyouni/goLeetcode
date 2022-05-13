d8d4e1b2e6f1153a2b3e0a2ea9163004ac56bb0f
hn hn <2125102013@stu.hqu.edu.cn>
2022-05-13 13:22:44 +0800

    5.13

--- src/main/38.有效的数独.go
+++ src/main/38.有效的数独.go
@@ -4,7 +4,7 @@ func isValidSudoku(board [][]byte) bool {
 	var rows, columns [9][9]int
 	var subboxes [3][3][9]int
 	for i, row := range board {
-		for j, c := range rows {
+		for j, c := range row {
 			if c == '.' {
 				continue
 			}
@@ -12,7 +12,7 @@ func isValidSudoku(board [][]byte) bool {
 			rows[i][index]++
 			columns[j][index]++
 			subboxes[i/3][j/3][index]++
-			if row[i][index] > 1 || columns[j][index] > 1 || subboxes[i/3][j/3][index] > 1 {
+			if rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i/3][j/3][index] > 1 {
 				return false
 			}
 		}
