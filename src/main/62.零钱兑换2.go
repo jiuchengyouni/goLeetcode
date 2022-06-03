acfa6aae9d8b3c234860e2b7a7b5ff0d6fdbcbb1
hn hn <2125102013@stu.hqu.edu.cn>
2022-06-04 02:18:35 +0800

    6.3

---
+++ src/main/62.零钱兑换2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func change(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1
+	for i := 0; i < len(coins); i++ {
+		for j := i; j < amount; j++ {
+			dp[i] += dp[i-coins[j]]
+		}
+	}
+	return dp[amount]
+}
+
+func main() {
+	amount := 4
+	coins := []int{1, 2, 5}
+	fmt.Println(change(amount, coins))
+}
